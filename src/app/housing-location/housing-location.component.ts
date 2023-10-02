d50d8cd7feecaf217ab55f8935331d91eaa87a33
Dan Stoakes <dan.stoakes@flauntdigital.com>
2023-10-02 15:49:37 +0100

    Use the routing parameters to move data between components

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 import { HousingLocation } from '../housinglocation';
 
@@ -7,13 +8,16 @@ import { HousingLocation } from '../housinglocation';
   selector: 'app-housing-location',
   standalone: true,
   imports: [
-    CommonModule
+    CommonModule,
+    RouterLink,
+    RouterOutlet
   ],
   template: `
     <section class="listing">
       <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
       <h2 class="listing-heading">{{ housingLocation.name }}</h2>
       <p class="listing-location">{{ housingLocation.city}}, {{housingLocation.state }}</p>
+      <a [routerLink]="['/details', housingLocation.id]">Learn More</a>
     </section>
   `,
   styleUrls: ['./housing-location.component.css']
