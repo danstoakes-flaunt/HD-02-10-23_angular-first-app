e9c9aa4a78025d87efefe9fbbef900ed5e191928
Dan Stoakes <dan.stoakes@flauntdigital.com>
2023-10-02 15:55:01 +0100

    Add support for Angular's form functionality and capture the data in the console

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -8,7 +8,7 @@ import { HousingLocation } from './housinglocation';
 export class HousingService 
 {
   readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
-  
+
   housingLocationList: HousingLocation[] = [
     {
       id: 0,
@@ -118,6 +118,10 @@ export class HousingService
   
   getHousingLocationById(id: number): HousingLocation | undefined {
     return this.housingLocationList.find(housingLocation => housingLocation.id === id);
+  }
+
+  submitApplication(firstName: string, lastName: string, email: string) {
+    console.log(`Homes application received: firstName: ${firstName}, lastName: ${lastName}, email: ${email}.`);
   }
 
   constructor () {
